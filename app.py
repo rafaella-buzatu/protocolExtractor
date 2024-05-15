9ccae96693ea57d69d9929cead049e70a42d6e5b
rafaella <rafaella7260@yahoo.ro>
2024-05-15 20:48:44 +0200

    fixing flask for google storage

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from processParticipantForm import handle_form as handle_participant_form
 from processProtocolForm import handle_form as handle_protocol_form
 import os
 from google.cloud import storage
+from google.oauth2 import service_account
 
 app = Flask(__name__)
 
@@ -42,16 +43,24 @@ def submit_protocol():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
+
+def get_gcs_client():
+    """Create a GCS client using credentials from an environment variable."""
+    credentials_info = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    if credentials_info:
+        credentials_dict = json.loads(credentials_info)
+        credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+    else:
+        raise EnvironmentError("Failed to retrieve GCS credentials")
+    client = storage.Client(credentials=credentials)
+    return client
+
 def upload_file_to_gcs(file, bucket_name, blob_name):
-    """Uploads a file to the bucket."""
-    # Create a Cloud Storage client.
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
+    """Uploads a file to the specified GCS bucket."""
+    client = get_gcs_client()
+    bucket = client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
-
-    # Upload the file to a destination
-    blob.upload_from_file(file, content_type=file.content_type)
-
+    blob.upload_from_file(file.stream, content_type=file.content_type)
     return blob.public_url
 
 @app.route('/save-file', methods=['POST'])
@@ -60,14 +69,14 @@ def save_file():
         file = request.files['file']
         if file.filename == '':
             return jsonify({'error': 'No selected file'}), 400
-        
-        # Define Google Cloud Storage bucket name
         bucket_name = os.environ.get('GCP_STORAGE_BUCKET')
-        
-        # Upload file to Google Cloud Storage
-        public_url = upload_file_to_gcs(file, bucket_name, file.filename)
-        
-        return jsonify({'message': f'File uploaded to {public_url}'})
+        if not bucket_name:
+            return jsonify({'error': 'Bucket name not configured'}), 500
 
+        public_url = upload_file_to_gcs(file, bucket_name, file.filename)
+        return jsonify({'message': f'File uploaded to {public_url}'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
